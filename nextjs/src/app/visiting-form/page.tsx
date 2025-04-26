2fbd5ed8feaa31962fed2f4f525c497ee44050f2
jonathanyangg <jonathan.yang.x@gmail.com>
2025-04-26 17:27:38 -0400

    removed private page from supabase

--- nextjs/src/app/visiting-form/page.tsx
+++ nextjs/src/app/visiting-form/page.tsx
@@ -5,7 +5,6 @@ import VisitingForm from '@/components/visiting-form/VisitingForm';
 export default function VisitingFormPage() {
   return (
     <div className="flex flex-col min-h-screen bg-white">
-      <Navbar />
       <main className="flex-1 container mx-auto px-4 py-8 max-w-4xl">
         <VisitingForm />
       </main>
